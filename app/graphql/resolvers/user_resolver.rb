f8ca7c04fceafc8c774500cbde75063435299a67
Brian Wong <brianw7218@gmail.com>
2024-05-16 14:34:31 -0700

    Add ability to query users by email

--- app/graphql/resolvers/user_resolver.rb
+++ app/graphql/resolvers/user_resolver.rb
@@ -3,9 +3,10 @@ module Resolvers
     type [Types::UserType], null: true
     argument :id, ID, required: false
     argument :username, String, required: false
+    argument :email, String, required: false
 
-    def resolve(id: nil, username: nil)
-      user = User.find_by_id(id).find_by_username(username)
+    def resolve(id: nil, username: nil, email: nil)
+      user = User.find_by_id(id).find_by_username(username).find_by_email(email)
       user.nil? ? [] : user.includes(:comments).all
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
 
   scope :find_by_id, ->(id) { where(id: id) if id.present? }
   scope :find_by_username, ->(username) { where(username: username) if username.present? }
+  scope :find_by_email, ->(email) { where(email: email) if email.present? }
 end
